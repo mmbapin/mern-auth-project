cc2382e38e05178b62ee54e68abfdd3a32c57cbe
mmbapin <bapinmm@gmail.com>
2026-04-19 22:05:04 +0600

    added login logout function in controller add route and test register login and logout api in postman

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,12 +38,78 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
-    res.json({
+    return res.json({
       success: true,
       message: "User registered successfully."
     })
   } catch (error) {
-    res.json({
+    return res.json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+
+export const login = async (req, res) => {
+  const {email, password} = req.body;
+  if(!email || !password){
+    return res.json({
+      success: false,
+      message: "Email and Password are required."
+    })
+  }
+  try {
+    const user = await User.findOne({email});
+    if(!user){
+      return res.json({
+        success: false,
+        message: "Invalid Email."
+      })
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+      return res.json({
+        success: false,
+        message: "Invalid Password."
+      })
+    }
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    })
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+    return res.json({
+      success: true,
+      message: "User logged in successfully."
+    })
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({
+      success: true,
+      message: "User logged out successfully."
+    })
+  } catch (error) {
+    return res.json({
       success: false,
       message: error.message
     })
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +12,12 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser({credentials: true}));
+
+///API Endpoints
+app.get('/', (req, res) => {
+    res.send("API Working");
+})
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
