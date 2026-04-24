2a47adf028654a239eb074b9d76c07fa11fa2756
mmbapin <bapinmm@gmail.com>
2026-04-24 23:43:35 +0600

    email verify added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,40 @@ import React, { use, useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
   const navigate = useNavigate()
   const {userData, backendUrl, setIsLoggedIn, setUserData} = useContext(AppContext);
+
+  const sendVerificationOtp = async () => {
+    try {
+      const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp', {withCredentials: true});
+      if(data.success){
+        toast.success(data.message);
+        navigate('/email-verify');
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const {data} = await axios.post(backendUrl + '/api/auth/logout');
+      if (data.success) {
+        setIsLoggedIn(false);
+        setUserData(null);
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6'>
       <img src={assets.logo} alt="logo" className='w-28 sm:w-32'/>
@@ -14,8 +44,8 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className='absolute hidden group-hover:block right-0 top-0 z-10 text-black rounded pt-10'>
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm '>
-              {!userData.isAccountVerified && <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>}
-              <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
+              {!userData.isAccountVerified && <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer' onClick={sendVerificationOtp}>Verify Email</li>}
+              <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
             </ul>
           </div>
         </div>
--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useRef, useContext } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const EmailVerify = () => {
-  return (
-    <div>EmailVerify</div>
-  )
-}
+  const inputRef = useRef([]);
+  axios.default.withCredentials = true;
+  const navigate = useNavigate();
+  const { backendUrl, isLoggedin, userData, getUserData } =
+    useContext(AppContext);
 
-export default EmailVerify
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRef.current.length - 1) {
+      inputRef.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRef.current[index - 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRef.current[index]) {
+        inputRef.current[index].value = char;
+      }
+    });
+  };
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const otpArray = inputRef.current.map((e) => e.value);
+      const otp = otpArray.join("");
+      const { data } = await axios.post(backendUrl + "/api/auth/verify-email", {
+        otp,
+      });
+
+      if (data.success) {
+        toast.success(data.message);
+        getUserData();
+        navigate("/");
+      } else {
+        toast.error(error.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-500">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <form
+        onSubmit={onSubmitHandler}
+        className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm"
+      >
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Email Verify OTP
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter the 6-digit code sent to your email id.
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRef.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full cursor-pointer">
+          Verify email
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EmailVerify;
